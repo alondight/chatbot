bc9542d9684563f200c1925a41f7991066cf16a6
박준혁 <alondightoo@naver.com>
2018-09-06 16:55:01 +0900

    'test'

--- app.js
+++ app.js
@@ -113,22 +113,34 @@ function sendTextMessage(recipientId, message) {
 					"attachment":{
 					  "type":"template",
 					  "payload":{
-						"template_type":"open_graph",
+						"template_type":"generic",
 						"elements":[
 						   {
-							"url":"https://thesmc.co.kr/",
+							"title":"Welcome!",
+							"image_url":"https://petersfancybrownhats.com/company_image.png",
+							"subtitle":"We have the right hat for everyone.",
+							"default_action": {
+							  "type": "web_url",
+							  "url": "https://petersfancybrownhats.com/view?item=103",
+							  "messenger_extensions": false,
+							  "webview_height_ratio": "tall",
+							  "fallback_url": "https://petersfancybrownhats.com/"
+							},
 							"buttons":[
 							  {
-								"type":"https://thesmc.co.kr/",
-								"url":"https://thesmc.co.kr/",
-								"title":"View More"
-							  }
-							]
+								"type":"web_url",
+								"url":"https://petersfancybrownhats.com",
+								"title":"View Website"
+							  },{
+								"type":"postback",
+								"title":"Start Chatting",
+								"payload":"DEVELOPER_DEFINED_PAYLOAD"
+							  }              
+							]      
 						  }
 						]
 					  }
 					}
-				}
 			}
 		}, function(error, response, body) {
 			if (error) {
